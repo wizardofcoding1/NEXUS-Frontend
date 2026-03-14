1b1f59a877d1ae77724930e66bed7781af8616eb
wizardofcoding1 <152683729+wizardofcoding1@users.noreply.github.com>
2026-03-14 14:16:57 +0530

    Resolved merge conflict

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react(),tailwindcss()],
-=======
-  plugins: [react(),tailwindcss()],base: '/',
->>>>>>> e10146984f554d5728d7023b097f180da318ef21
+  plugins: [react(), tailwindcss()],
+  base: '/',
 })
